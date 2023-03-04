aef2273db79937678f1fbb03392681195864bf11
Alberto <ramirez-alberto@zoho.com>
2023-03-04 01:48:40 -0800

    Add serialise and deserialise operations

--- cancionesweb/modelos/modelos.py
+++ cancionesweb/modelos/modelos.py
@@ -1,10 +1,13 @@
+import typing
 import enum
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
 
 db = SQLAlchemy()
 
 album_cancion = db.Table('album_cancion',
-    db.Column('album_id', db.Integer, db.ForeignKey('album.id', primary_key = True)),
+    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key = True),
     db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key = True))
 
 class Cancion(db.Model):
@@ -24,6 +27,12 @@ class Medio(enum.Enum):
     CD = 3
 
 class Album(db.Model):
+    ''' Represents an album table.\n
+            titulo = string,\n
+            ano = int\n
+            description = string\n
+            medio = Medio
+    '''
     __tablename__ = 'album'
 
     id = db.Column(db.Integer, primary_key = True)
@@ -32,6 +41,7 @@ class Album(db.Model):
     descripcion = db.Column(db.String(150))
     medio = db.Column(db.Enum(Medio))
 
+    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
     canciones = db.relationship('Cancion', secondary = 'album_cancion', back_populates="albumes")
 
 class Usuario(db.Model):
@@ -42,3 +52,28 @@ class Usuario(db.Model):
     contrasena = db.Column(db.String(50))
 
     albumes = db.relationship('Album', cascade = 'all, delete, delete-orphan')
+
+class EnumerateToDict(fields.Field):
+    def _serialize(self, value: typing.Any, attr: str | None, obj: typing.Any, **kwargs):
+        if value is None:
+            return None
+        return {"llave": value.name, "valor" : value.value}
+
+class AlbumSchema(SQLAlchemyAutoSchema):
+    medio = EnumerateToDict(attribute=('medio'))
+    class Meta:
+        model = Album
+        include_relationships = True
+        load_instance = True
+
+class CancionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Cancion
+        include_relationships = True
+        load_instance = True
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+        include_relationships = True
+        load_instance = True
