b8633c93f4eafc2dab34c91b92af7bc6a0c4d139
Alberto <ramirez-alberto@zoho.com>
2023-03-04 01:51:23 -0800

    Test album serialisation

--- cancionesweb/app.py
+++ cancionesweb/app.py
@@ -4,5 +4,10 @@ from modelos import db
 app = create_app()
 db.init_app(app)
 with app.app_context():
-    db.create_all()
+    from modelos import Album, Medio, AlbumSchema
+    a = Album(titulo="Prueba", ano=1999, descripcion="Lorem Ipsum", medio=Medio.CASETE)
+    a_schema = AlbumSchema()
+    db.session.add(a)
+    db.session.commit()
 
+    print([a_schema.dump(a) for a in Album.query.all()])
