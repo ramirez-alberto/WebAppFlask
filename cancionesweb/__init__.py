73495f8622ffc848623777f200c897f970d0d523
Ramirez <ramirez.alberto@zoho.com>
2023-03-06 20:26:20 -0800

    Add CORS

--- cancionesweb/__init__.py
+++ cancionesweb/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from flask-cors import CORS, cross_origin
+from flask_cors import CORS
 from .vistas import (VistaAlbumes, VistaAlbum, VistaAlbumUsuario, VistaCancion,
     VistaAlbumCancion,VistaCanciones, VistaSignIn, VistaLogin)
 from .modelos import db
